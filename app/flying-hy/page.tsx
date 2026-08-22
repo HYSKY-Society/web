1332dace1f446fbb1959668649af05b4d8ab8a3e
deemack918 <134224064+deemack918@users.noreply.github.com>
2026-08-22 01:40:46 -0500

    Add Joshua Heyne speaker headshot

--- app/flying-hy/page.tsx
+++ app/flying-hy/page.tsx
@@ -44,6 +44,7 @@ const speakerHeadshots: Record<string, string | string[]> = {
   'Dr. Eva Maleviti': '1tUwQYn9kWYRBV4JDcbWyhWnWRafafbJ5',
   'Jared Semik': '16iAjjBDhU1x5c5nEmV6tBqL0zay1qrip',
   'Michael Bluy': '1pVfYMH_R41Bcsk7TnNk8dkw3c-STfVDz',
+  'Joshua Heyne': '1DvSNgqWKWX_1NXhkf0Lk09WBF50cC243',
   'Mark van Wyk': '1ICYFaG8wUlVQCV9SKONF1jwLQtiKWlih',
   'Martin Chan': '1i4pljgjeiM_mvUBxw3x9WOsqfCyVu9cC',
   'Mikael Cardinal': '1QTWD7i57uRMQWy7Bc-0vDOLZZaOr1Vnv',
